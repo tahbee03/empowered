0214dbb27d88a35b3cdff6a61e6a1dd986355139
Haseebzz <chaudhuryhaseeb35@gmail.com>
2024-02-03 20:47:50 -0500

    added register page

---
+++ src/app/register/page.jsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      body: JSON.stringify({name, email, password}),
+      headers: {'Content-Type': 'application/json'},
+    });
+    if (response.ok) {
+      setUserCreated(true);
+    }
+    else {
+      setError(true);
+    }
+    setCreatingUser(false);
+  }
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+        <h1 className="text-xl font-bold my-4">Register</h1>
+
+        <form onSubmit={handleFormSubmit} className="flex flex-col gap-3">
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Full Name"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+            Register
+          </button>
+
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
+
+          <Link className="text-sm mt-3 text-right" href={"/login"}>
+            Already have an account? <span className="underline">Login</span>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
