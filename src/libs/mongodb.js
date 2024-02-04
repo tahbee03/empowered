0f46b1949315fc79a5c27c7a64f1f49bd59c78bc
Haseebzz <chaudhuryhaseeb35@gmail.com>
2024-02-03 20:48:19 -0500

    mongodb connection

---
+++ src/libs/mongodb.js
@@ -0,0 +1,29 @@
+// mongodb.js
+import mongoose from 'mongoose';
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectMongoDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connectMongoDB;
