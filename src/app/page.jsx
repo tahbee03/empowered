826fbeb196a29cb2c8f8085790336025f76c3820
Talike <talikeb20@gmail.com>
2024-02-12 22:15:33 -0500

    added basic elements to the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,36 @@
 import Image from 'next/image'
+import './home.css'
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello This is the homepage</h1>
+      <div className='center-cont'>
+        <Image
+          src='/logo.png'
+          alt='EmpowerEd logo'
+          width={500}
+          height={500}
+        />
+        <p>Unleashing potential with EmpowerEd resumes</p>
+        <div className='row'>
+          <Image
+            src='/filler1.png'
+            alt='paper'
+            width={200}
+            height={200}
+          />
+          <p>Enhance your resume with the power of AI</p>
+        </div>
+        <div className='row'>
+          <Image
+            src='/filler2.png'
+            alt='man in suit'
+            width={200}
+            height={200}
+          />
+          <p>Prepare yourself for a job that suits you</p>
+        </div>
+      </div>
     </main>
   )
 }
