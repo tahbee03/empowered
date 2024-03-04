257de8a07b3742988f69e333a29183941060e914
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2024-03-03 19:29:39 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '../components/Providers'
 import Header from '../components/Header'
-
+import Navbar from "../components/Navbar"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,9 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} dark:bg-medium`}>
         <AuthProvider>
-          <Header />
+          <Navbar/>
           <main>
             {children}
           </main>
