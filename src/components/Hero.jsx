16e75dd993060fcb9be061e8e3a4d07241de6282
Talike <talikeb20@gmail.com>
2024-03-07 20:40:24 -0500

    small changes (test)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ const Hero = () => {
   return (
     <div className='w-full bg-white py-16 px-4 shadow-md dark:bg-medium'>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-      <Image src="/filler2.png" alt="feature 1" width={400} height={200} />
+        <Image src="/filler2.png" alt="feature 1" width={400} height={200} />
         <div className='flex flex-col justify-center'>
           <p className='text-red-500 font-bold '>Best AI Resume Builder</p>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Welcome to Empowered</h1>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Welcome to EmpowerEd</h1>
           <p>
-          Step into a new era of resume design with our AI-powered platform. 
-  Create a resume that highlights your achievements and showcases your potential, 
-  all with the ease and precision of artificial intelligence.
+            Step into a new era of resume design with our AI-powered platform.
+            Create a resume that highlights your achievements and showcases your potential,
+            all with the ease and precision of artificial intelligence.
           </p>
           <button className='text-red-500 border border-red-500 hover:bg-red-500 hover:text-white px-4 py-2 rounded-full mx-2 w-[200px]'>Get Started</button>
         </div>
